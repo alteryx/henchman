46f288aa2aa89a5e9c8c148864c6f959ee7dd96c
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-07-17 16:23:40 -0400

    update to version 0.0.2

--- henchman/__init__.py
+++ henchman/__init__.py
@@ -11,4 +11,4 @@ Package is under active development.
 
 __author__ = '''Feature Labs Team'''
 __email__ = 'team@featurelabs.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/featurelabs/henchman',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
