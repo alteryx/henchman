7c39f8cb5625eb826076f92f2157854957df5e25
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-08-27 15:05:21 -0400

    add support for circle and conda

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -11,15 +12,26 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy>=1.13.3',
-                'pandas>=0.20.3',
+requirements = ['pandas>=0.20.3',
                 'bokeh>=0.12.16',
                 'scikit-learn>=0.19.1',
-                'networkx>=2.1']
+                'networkx>=2.1',
+                'numpy>=1.13.3']
 
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
+
+
+class build_ext(_build_ext):
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 setup(
     author="Feature Labs Team",
