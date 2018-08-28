42b5c2e8a8e9d6f8311662329bfa13ccdc3a6e2e
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-08-28 09:33:52 -0400

    remove support for 3.7 (for now)

--- setup.py
+++ setup.py
@@ -22,34 +22,21 @@ setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
 
-
-class build_ext(_build_ext):
-
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
-
 setup(
     author="Feature Labs Team",
     author_email='team@featurelabs.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     description="A collection of utility functions for making demo notebooks.",
     install_requires=requirements,
-    license="GNU General Public License v3",
+    license="BSD 3-clause",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='henchman',
