db28131836348b33f861f3fb297cd2ae62e65bcc
Seth Rothschild <seth.j.rothschild@gmail.com>
2018-09-25 14:38:40 -0400

    Correct path to templates directory (#33)

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -43,7 +43,7 @@ ipython_execlines = ["import pandas as pd",
                      "pd.set_option('display.width', 1000000)"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
