e6409cfa6196295a167edcf70e5b8dcdf586320c
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-09-25 13:18:04 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/featurelabs/henchman',
-    version='0.0.3.1',
+    version='0.0.4',
     zip_safe=False,
 )
